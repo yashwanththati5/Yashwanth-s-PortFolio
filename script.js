3b3b0c6bb7acf2dd4c43e5c1c54856257c5eaa09
yashwanth <58801238+yashwanththati5@users.noreply.github.com>
2023-02-13 00:38:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+// Dark Mode / light mode
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
+let doctitle = document.title;
+window.addEventListener("blur" , () => {
+  document.title = "Come Back :)";
+})
+window.addEventListener("focus" , () => {
+  document.title = doctitle;
+})
+
+                                    
